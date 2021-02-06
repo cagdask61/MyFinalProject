85ab9bc89c5844481a4b94e0f26cecda4e436132
kocamancagdas59@gmail.com <kocamancagdas59@gmail.com>
2021-02-07 00:01:47 +0300

    9.Gün dersi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,12 +11,24 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             //ProductTest();
+            //CategoryTest();
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetail())
+            {
+                Console.WriteLine(product.ProductName+" / " + product.CategoryName);
+            }
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
         }
 
         private static void ProductTest()
