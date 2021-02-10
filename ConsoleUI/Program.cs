329f79ab145991e07276abeb2abdd79c77710dfe
Cagdas <74134486+cagdask61@users.noreply.github.com>
2021-02-11 00:04:42 +0300

    10 Şubat Ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,35 +10,30 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
-            //CategoryTest();
-
+            
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetail())
+            var result = productManager.GetProductDetail();
+            if (result.Success==true)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / "+product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            foreach (var product in productManager.GetProductDetail().Data)
             {
                 Console.WriteLine(product.ProductName+" / " + product.CategoryName);
             }
 
         }
 
-        private static void CategoryTest()
-        {
-            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
-            {
-                Console.WriteLine(category.CategoryName);
-            }
-        }
-
-        private static void ProductTest()
-        {
-            ProductManager productManager = new ProductManager(new EfProductDal());
-
-            foreach (var product in productManager.GetByUnitPrice(50, 100))
-            {
-                Console.WriteLine(product.ProductId + " -- " + product.CategoryId + " -- " + product.ProductName);
-            }
-        }
+       
+        
     }
 }
