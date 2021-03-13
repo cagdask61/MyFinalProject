238f7b278f1efe0975b03d04ef145034b59ee65b
Cagdas <74134486+cagdask61@users.noreply.github.com>
2021-03-13 23:59:31 +0300

    23.59 -- 13/03/2021 -- C#

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -27,7 +27,6 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
-            Thread.Sleep(2000);
             var result = _productService.GetAll();
             if (result.Success)
             {
@@ -55,6 +54,17 @@ namespace WebAPI.Controllers
         {
             var result = _productService.Add(product);
 
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbycategory")]
+        public IActionResult GetBycategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
             if (result.Success)
             {
                 return Ok(result);
